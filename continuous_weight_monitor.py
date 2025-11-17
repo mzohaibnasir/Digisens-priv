f1691e4209e1922e5a42e656314f0281b751518e
Zohaib <m.zohaibnasir6@gmail.com>
2025-11-17 23:38:40 +0500

    lol

--- continuous_weight_monitor.py
+++ continuous_weight_monitor.py
@@ -243,7 +243,7 @@ def main():
         {'id': '0120221125101002', 'label': 'MUX 1', 'extended': True},
         {'id': '0120221125084905', 'label': 'MUX 2', 'extended': True},
         {'id': '0120250919084626', 'label': 'MUX 3', 'extended': True},
-        {'id': '0120221125075550', 'label': 'MUX 4', 'extended': False},  # Standard mode: last 3 digits
+        {'id': '0120221124064344', 'label': 'MUX 4', 'extended': True},  # Standard mode: last 3 digits
         {'id': '0120250925110711', 'label': 'MUX 5', 'extended': True},
     ]
 
